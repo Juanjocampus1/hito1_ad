bf5f1301409a0e23b86856f22a25c17a62534426
Juanjo <juanjose.acebedolara.23@campusfp.es>
2025-01-27 23:39:11 +0100

    initial commit

---
+++ src/main/java/com/empresa/BackAccesoDatosApplication.java
@@ -0,0 +1,13 @@
+package com.empresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackAccesoDatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackAccesoDatosApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/empresa/Controller/Controller.java
@@ -0,0 +1,4 @@
+package com.empresa.Controller;
+
+public class Controller {
+}
